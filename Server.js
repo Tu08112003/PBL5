b9173d9d06d50649812fbe04a59b139970030816
Nhung <hongnhung16052003@gmail.com>
2024-05-10 17:02:31 +0700

    fix: send data to app

--- Server.js
+++ Server.js
@@ -8,35 +8,50 @@ const server = http.createServer(app);
 const wss = new WebSocket.Server({ server });
 
 let locationData = null;
-
+let ipData = null;
 app.use(bodyParser.urlencoded({ extended: true }));
 
 // Lắng nghe sự kiện kết nối mới từ client
 wss.on('connection', (ws) => {
     // Gửi dữ liệu vị trí hiện tại cho client mới kết nối
-    if (locationData !== null) {
-        ws.send(JSON.stringify(locationData));
-    }
+     const combinedData = {
+         location: locationData,
+         ip: ipData // Assuming ipData is an object with 'ip' property
+    };
+    console.log(combinedData);
+     ws.send(JSON.stringify(combinedData));
+
+    // console.log("Chưa gửi")
 
     // Lắng nghe sự kiện đóng kết nối từ client
-    ws.on('close', () => {
-        console.log('Client disconnected');
+    ws.on("close", () => {
+        console.log("Client disconnected");
     });
 });
 
 // Hàm gửi dữ liệu mới đến tất cả các client thông qua WebSocket
 function sendLocationUpdate() {
-    wss.clients.forEach(function each(client) {
-        if (client.readyState === WebSocket.OPEN) {
-            client.send(JSON.stringify(locationData));
-        }
-    });
+        const combinedData = {
+            location: locationData,
+            ip: ipData, // Assuming ipData is an object with 'ip' property
+    };
+    console.log(combinedData)
 }
 
+app.post('/send_ip_esp', (req, res) => {
+    const {ip} = req.body;
+    console.log('Received ip data:', {ip});
+    ipData = { ip }
+    // // Gửi thông báo cập nhật đến tất cả các client
+    // sendLocationUpdate();
+    res.status(200).send('ip data received successfully');
+});
+
 app.post('/send_location', (req, res) => {
-    const { latitude, longitude } = req.body;
-    console.log('Received location data:', { latitude, longitude });
+    const { latitude, longitude} = req.body;
+    console.log('Received location data:', { latitude, longitude});
     locationData = { latitude, longitude };
+    // console.log(locationData)
     // Gửi thông báo cập nhật đến tất cả các client
     sendLocationUpdate();
     res.status(200).send('Location data received successfully');
